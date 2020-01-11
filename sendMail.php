4b26db5aa042d50a0eea111527b7fbb922444814
StudentScalda228842 <59051008+StudentScalda228842@users.noreply.github.com>
2020-01-11 18:36:00 +0100

    Add files via upload

---
+++ sendMail.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+$name = $_POST['Name '];
+
+  $visitor_email = $_POST['Company '];
+
+  $message = $_POST['Message '];
+
+
+$email_from = 'yourname@yourwebsite.com';
+$email_subject = "New Form submission";
+
+
+
+$email_body = "You have received a new message from the user $Name.\n".
+
+                            "Here is the message:\n $Message".
+
+
+ 
